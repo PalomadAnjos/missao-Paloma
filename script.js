9d3b77aacd1c0c13b05dcb56ba983b09d20f3e1e
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2024-06-04 12:01:33 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultadol");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {//primeiro.objetivo da lista de perguntas 
+      enunciado:"pergunta1 , alternativas: ["alternativas1"] , 
+ "altrnativas2"]
+ },
+ {//segundo objeto da lista de perguntas 
+    enunciado: "perguntas2" ,
+    alternativas ["alternativas1" , "alternativas2"]
+ }
+
